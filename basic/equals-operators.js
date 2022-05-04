c5ff746bcc0f968cbbe293936b311d87d6e94f47
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-03 21:25:33 -0300

    add equals operators

---
+++ basic/equals-operators.js
@@ -0,0 +1,14 @@
+console.log("text" ? true : false); // true - if text is not null or undefined
+console.log("text" == true); //
+
+console.log("text" === true); // false
+console.log("text" === "text"); // true
+
+const person1 = {
+  name: "John",
+};
+const person2 = {
+  name: "John",
+};
+
+console.log(person1 === person2); // false - because they are not the same object
