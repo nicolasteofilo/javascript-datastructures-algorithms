4009aa1a93586d751896aaad06f5c3ad8759c971
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-03 21:50:38 -0300

    add fucntions

---
+++ basic/fucntion.js
@@ -0,0 +1,15 @@
+function sayHello() {
+  console.log("Hello World");
+}
+
+function output(message) {
+  console.log(message);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+sayHello();
+output("Message");
+console.log(sum(1, 4));
