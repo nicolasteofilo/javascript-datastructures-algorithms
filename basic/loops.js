66db100716b85a34bc980b097e57551f75246ba1
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-03 21:45:09 -0300

    add loops

---
+++ basic/loops.js
@@ -0,0 +1,19 @@
+console.log("FOR LOOP");
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log("WHILE LOOP");
+var i = 0;
+// the while executes the code inside the while loop until the condition is true
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+console.log("DO WHILE LOOP");
+var j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 10);
