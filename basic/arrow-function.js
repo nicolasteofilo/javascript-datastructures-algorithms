1e69e090ecff101def705da2b38113fd40552ae8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-04 22:02:57 -0300

    add arrows function

---
+++ basic/arrow-function.js
@@ -0,0 +1,10 @@
+var circleAreaES5 = function (r) {
+  return Math.PI * r * r;
+};
+
+const circleArea = (r) => {
+  return Math.PI * r * r;
+};
+
+console.log(circleAreaES5(2));
+console.log(circleArea(2));
