723c00b249672323e8444135fdcb16bc7aa6d1d5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-04 23:10:38 -0300

    add modules

---
+++ basic/modules/calcArea.js
@@ -0,0 +1,7 @@
+export function calcCircle(r) {
+  return Math.PI * r * r;
+}
+
+export function calcSquare(a) {
+  return a * a;
+}
---
+++ basic/modules/index.js
@@ -0,0 +1,3 @@
+import { calcCircle, calcSquare } from "./calcArea.js";
+console.log(calcCircle(2));
+console.log(calcSquare(3));
