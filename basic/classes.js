8ebcb38e4604798c6b9dbef5096b9f302e98fd59
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-04 22:23:19 -0300

    add heritage

--- basic/classes.js
+++ basic/classes.js
@@ -12,3 +12,19 @@ class Book {
 
 const book = new Book("The Hobbit", 300, "123456789");
 book.print();
+
+// Heritage
+class ITBook extends Book {
+  constructor(title, pages, isbn, tech) {
+    // o super referencia ao construtor da classe pai ( Book )
+    super(title, pages, isbn);
+    this.tech = tech;
+  }
+
+  printTech() {
+    console.log(`${this.title} is written in ${this.tech}`);
+  }
+}
+
+const jsBook = new ITBook("JavaScript", 300, "123456789", "JavaScript");
+jsBook.printTech();
