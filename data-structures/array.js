a1eb3a7120a35ccd23015eb5d637d617d54184de
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-09 21:08:49 -0300

    create insertFirstPosition function in array

--- data-structures/array.js
+++ data-structures/array.js
@@ -23,3 +23,17 @@ for (let i = 3; i < 20; i++) {
 for (let i = 1; i < fibonacci.length; i++) {
   console.log(fibonacci[i]);
 }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.push(11);
+console.log(numbers);
+
+Array.prototype.insertFirstPosition = function (value) {
+  for (let i = this.length; i >= 0; i--) {
+    this[i] = this[i - 1];
+  }
+
+  this[0] = value;
+};
+numbers.insertFirstPosition(-1);
+console.log(numbers);
