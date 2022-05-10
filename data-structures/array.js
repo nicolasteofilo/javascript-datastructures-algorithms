618e3cec9ff75a54cfc7a51da8d96d4e5b796060
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-09 21:41:07 -0300

    add elements in array with splice

--- data-structures/array.js
+++ data-structures/array.js
@@ -24,9 +24,8 @@ for (let i = 1; i < fibonacci.length; i++) {
   console.log(fibonacci[i]);
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 numbers.push(11);
-console.log(numbers);
 
 Array.prototype.insertFirstPosition = function (value) {
   for (let i = this.length; i >= 0; i--) {
@@ -35,8 +34,59 @@ Array.prototype.insertFirstPosition = function (value) {
 
   this[0] = value;
 };
-numbers.insertFirstPosition(-1);
+
+numbers.insertFirstPosition(-1); // add -1 to the first position
 console.log(numbers);
 
-numbers.unshift(-2);
+numbers.unshift(-2); // add -2 to the first position
+numbers.unshift(-4, -3);
 console.log(numbers);
+
+numbers.pop(); // removes the last element
+console.log(numbers);
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] = numbers[i + 1]; // moves the elements to the left
+}
+console.log(numbers);
+
+Array.prototype.reIndex = function (array) {
+  const newArray = new Array();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== undefined) {
+      newArray.push(array[i]);
+    }
+  }
+};
+
+Array.prototype.removeFistPosition = function () {
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i + 1];
+  }
+  return this.reIndex(this);
+};
+
+const citys = [
+  "New York",
+  "Los Angeles",
+  "Chicago",
+  "Houston",
+  "Philadelphia",
+  "Phoenix",
+  "San Antonio",
+  "San Diego",
+  "Dallas",
+  "San Jose",
+];
+
+console.log(citys.shift()); // removes the first element
+console.log(citys);
+citys.splice(0, 1); // removes the first element
+console.log(citys);
+
+function addElement(array, position, ...elements) {
+  array.splice(position, 0, ...elements);
+}
+
+addElement(citys, 2, "Detroit");
+console.log(citys);
