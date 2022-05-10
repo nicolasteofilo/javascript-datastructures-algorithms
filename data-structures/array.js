8f58658d3c8eab3a83a14529e5f79f5b3acd5670
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-09 22:08:14 -0300

    bisimensional arrays

--- data-structures/array.js
+++ data-structures/array.js
@@ -90,3 +90,20 @@ function addElement(array, position, ...elements) {
 
 addElement(citys, 2, "Detroit");
 console.log(citys);
+
+// --- BIDIMENSIONAL ARRAY ---
+// array in array, it's called a bidimensional array
+let averageTemp = [];
+averageTemp[0] = [72, 75, 79, 79, 81, 81];
+averageTemp[1] = [81, 79, 75, 75, 73, 73];
+
+function printMatrix(myMatrix) {
+  for (let i = 0; i < myMatrix.length; i++) {
+    for (let j = 0; j < myMatrix[i].length; j++) {
+      console.log(myMatrix[i][j]);
+    }
+  }
+}
+
+printMatrix(averageTemp);
+// console.table(averageTemp);
