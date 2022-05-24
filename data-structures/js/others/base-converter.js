541b39c5ceba7322c6cd57c4c4fe1e810bae0b7c
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-23 22:16:02 -0300

    feat: add enqueue in Queue

--- data-structures/js/others/base-converter.js
+++ data-structures/js/others/base-converter.js
@@ -26,12 +26,20 @@ function baseConverter(decNumber, base) {
   let number = decNumber;
   let rem;
   let baseString = "";
+
   if (!(base >= 2 && base <= 36)) {
     return "";
   }
+
   while (number > 0) {
     rem = Math.floor(number % base);
     remStack.push(rem);
     number = Math.floor(number / base);
   }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()];
+  }
+
+  return baseString;
 }
---
+++ examples/chapter05/01-Queue.js
@@ -0,0 +1,12 @@
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++; // ==> this.count = this.count + 1
+  }
+}
