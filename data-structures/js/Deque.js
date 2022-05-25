a84f0ce9c8d3d59b6ca4a7dee6c04576b08c9dd1
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-24 22:30:49 -0300

    feat: add addFront method in Deque

---
+++ data-structures/js/Deque.js
@@ -0,0 +1,32 @@
+export class Deque {
+  constructor() {
+    this.count = 0
+    this.lowestCount = 0
+    this.items = []
+  }
+
+  size() {
+    return this.count - this.lowestCount
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  addFront(element) {
+    if(this.isEmpty()) {
+      this.addBack(element)
+    } else if(this.lowestCount > 0) {
+      this.lowestCount--
+      this.items[this.lowestCount] = element
+    } else {
+      for(let i; this.count > 0; i--) {
+        this.items[i] = this.items[i - 1]
+      }
+
+      this.count++
+      this.lowestCount = 0
+      this.items[0] = element
+    }
+  }
+}
