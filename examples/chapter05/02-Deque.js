d4a374e31d310bb4c67ddac726ceb363c395eb18
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-24 22:48:59 -0300

    feat: create Deque example

---
+++ examples/chapter05/02-Deque.js
@@ -0,0 +1,16 @@
+import { Deque } from '../../data-structures/js/Deque.js'
+
+const deque = new Deque()
+console.log(deque.isEmpty())
+deque.addBack('John')
+deque.addBack('Jack')
+console.log(deque.toString())
+deque.addBack('Camila')
+console.log(deque.toString())
+console.log(deque.size())
+deque.removeFront()
+console.log(deque.toString())
+deque.removeBack()
+console.log(deque.toString())
+deque.addFront('John')
+console.log(deque.toString())
