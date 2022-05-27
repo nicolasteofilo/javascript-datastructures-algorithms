6b43bf860a5307a163af5308bae8aa0d12725e67
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-26 23:02:13 -0300

    feat: use a palindrome function

--- examples/chapter05/Palindrome-Checker.js
+++ examples/chapter05/Palindrome-Checker.js
@@ -1,14 +1,15 @@
 import { Deque } from '../../data-structures/js/Deque.js'
 
 function palindromeChecer(aString) {
-  if(!!aString || aString.length ) {
+  if(aString === undefined || (aString !== null && aString.length === 0) || aString === null) {
     return false
   }
 
   const deque = new Deque()
   const formatedString = aString.toLocaleLowerCase().split('').join('')
   let isEqual = true
-  let fistChar, lastChar
+  let fistChar
+  let lastChar
 
   for(let i = 0; i < formatedString.length; i++) {
     deque.addBack(formatedString.charAt(i))
@@ -18,9 +19,14 @@ function palindromeChecer(aString) {
     fistChar = deque.removeFront()
     lastChar = deque.removeBack()
     if(fistChar !== lastChar) {
-      return false
+      isEqual = false
     }
   }
 
   return isEqual
 }
+
+console.log("'a' is palindrome?", palindromeChecer('a'))
+console.log("'nicolas' is palindrome?", palindromeChecer('nicolas'))
+console.log("'level' is palindrome?", palindromeChecer('level'))
+console.log("'kayak' is palindrome?", palindromeChecer('kayak'))
