ee944ff79acf846641a6083f9f672510dee36c65
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-07 21:43:03 -0300

    feat: add insert method in Linked list

--- js/data-structures/Linked-list.js
+++ js/data-structures/Linked-list.js
@@ -49,6 +49,25 @@ export default class LinkedList {
     }
     return undefined;
   }
+
+  inset(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        const current = node;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
 }
 
 const list = new LinkedList();
