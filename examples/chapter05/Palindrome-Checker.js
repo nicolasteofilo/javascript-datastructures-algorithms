84b1e324fe28bd4d241e43d4a73c77e0f4ded777
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-09 22:49:49 -0300

    feat: add insert method in doubly linked list

--- examples/chapter05/Palindrome-Checker.js
+++ examples/chapter05/Palindrome-Checker.js
@@ -1,32 +1,36 @@
-import { Deque } from '../../data-structures/js/Deque.js'
+import { Deque } from "../../js/data-structures/deque.js";
 
-function palindromeChecer(aString) {
-  if(aString === undefined || (aString !== null && aString.length === 0) || aString === null) {
-    return false
+function palindromeChecker(aString) {
+  if (
+    aString === undefined ||
+    (aString !== null && aString.length === 0) ||
+    aString === null
+  ) {
+    return false;
   }
 
-  const deque = new Deque()
-  const formatedString = aString.toLocaleLowerCase().split('').join('')
-  let isEqual = true
-  let fistChar
-  let lastChar
+  const deque = new Deque();
+  const formatedString = aString.toLocaleLowerCase().split("").join("");
+  let isEqual = true;
+  let fistChar;
+  let lastChar;
 
-  for(let i = 0; i < formatedString.length; i++) {
-    deque.addBack(formatedString.charAt(i))
+  for (let i = 0; i < formatedString.length; i++) {
+    deque.addBack(formatedString.charAt(i));
   }
 
-  while(deque.size() > 1 && isEqual) {
-    fistChar = deque.removeFront()
-    lastChar = deque.removeBack()
-    if(fistChar !== lastChar) {
-      isEqual = false
+  while (deque.size() > 1 && isEqual) {
+    fistChar = deque.removeFront();
+    lastChar = deque.removeBack();
+    if (fistChar !== lastChar) {
+      isEqual = false;
     }
   }
 
-  return isEqual
+  return isEqual;
 }
 
-console.log("'a' is palindrome?", palindromeChecer('a'))
-console.log("'nicolas' is palindrome?", palindromeChecer('nicolas'))
-console.log("'level' is palindrome?", palindromeChecer('level'))
-console.log("'kayak' is palindrome?", palindromeChecer('kayak'))
+console.log("'a' is palindrome?", palindromeChecer("a"));
+console.log("'nicolas' is palindrome?", palindromeChecer("nicolas"));
+console.log("'level' is palindrome?", palindromeChecer("level"));
+console.log("'kayak' is palindrome?", palindromeChecer("kayak"));
