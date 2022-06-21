f81d4b1bf6bc06f009ec28f8e2055d7c93809697
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-20 21:24:09 -0300

    feat: add 'add' method in Set

--- js/data-structures/set.js
+++ js/data-structures/set.js
@@ -2,8 +2,16 @@ export class Set {
   constructor() {
     this.items = {}
   }
-  
+
   has(element) {
     return Object.prototype.hasOwnProperty(this.items, element)
+  }
+
+  add(element) {
+    if(!this.has(element)) {
+      this.items[element] = element
+      return true
+    }
+    return false
   }
 } 
