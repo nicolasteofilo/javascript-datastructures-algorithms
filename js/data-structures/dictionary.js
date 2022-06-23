c2af0900dea94dea8304cfa67085a48655ab3602
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-22 21:38:56 -0300

    feat: add set method in Dictionary

--- js/data-structures/dictionary.js
+++ js/data-structures/dictionary.js
@@ -1,11 +1,35 @@
-import { defaultToString } from '../utils.js'
+import { defaultToString } from "../utils.js";
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`
+  }
+}
+
 export class Dictionary {
   constructor(toStrFn = defaultToString) {
-    this.toStrFn = toStrFn
-    this.table = {}
+    this.toStrFn = toStrFn;
+    this.table = {};
   }
 
   hasKey(key) {
-    return this.table[key] != null
+    return this.table[key] != null;
+  }
+
+  set(key, value) {
+    if (key != null && value != null) {
+      const tableKey = this.toStrFn(key);
+      this.table[tableKey] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
   }
 }
+
+const dictionary = new Dictionary()
+dictionary.set('1', '@nicolasteofilo')
