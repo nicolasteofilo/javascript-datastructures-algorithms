2d5271406416e4f7834fba4f47a1cde756c5456d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-26 16:00:39 -0300

    feat: add remove method in HashTable

--- js/data-structures/hash-table.js
+++ js/data-structures/hash-table.js
@@ -24,19 +24,42 @@ export class HashTable {
     }
 
     /** @type {String} */
-    const tableKey = this.toStrFn(key)
-    let hash = 0
-    for(let i = 0; i < tableKey.length; i++) {
-      hash += tableKey.charCodeAt(i)
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
     }
-    return hash
+    return hash;
   }
 
   hashCode(key) {
-    return this.loseloseHashCode(key)
+    return this.loseloseHashCode(key);
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      this.table[position] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+
+  remove(key) {
+    const hash = this.hashCode(key);
+    const valuesPair = this.table[hash];
+    if (valuesPair != null) {
+      delete this.table[hash];
+      return true;
+    }
+    return false;
+  }
+
+  get(key) {
+    const valuePair = this.table[this.hashCode(key)];
+    return valuePair == null ? undefined : valuePair.value;
   }
 }
 
-const hashTable = new HashTable()
-const hashNicolas = hashTable.hashCode('nicolas')
-console.log(hashNicolas);
+const hashTable = new HashTable();
+hashTable.put("instagram", "nicolasteofilo");
