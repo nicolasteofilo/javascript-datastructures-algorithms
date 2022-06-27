89585badeb100712117244654e0296af84cc860b
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-27 20:06:19 -0300

    feat: add put in HashTableSeparateChainig

---
+++ js/data-structures/hash-table-sepate-chainig.js
@@ -0,0 +1,51 @@
+import { defaultToString } from "../utils.js";
+import { LinkedList } from "./linked-list.js";
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
+class HashTableSeparateChainig {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  loseloseHashCode(key) {
+    if (typeof key === "number") {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      if (this.table[position] == null) {
+        this.table[position] = new LinkedList();
+      }
+      this.table[position].push(new ValuePair(key, value));
+      return true;
+    }
+    return false;
+  }
+}
+
+const hash = new HashTableSeparateChainig();
+hash.put("insta", "@onicolasteofilo");
