f405ef698ebe3207b3f4ab91afe16a7e6e2eb7e5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-27 21:41:52 -0300

    feat: add get method in HashTableLinearProbing

--- js/data-structures/hash-table-linear-probing.js
+++ js/data-structures/hash-table-linear-probing.js
@@ -26,9 +26,10 @@ export class HashTableLinearProbing {
   put(key, value) {
     if (key != null && value != null) {
       const position = this.hashCode(key);
-      if (this.table[position] == null) {
+      if (this.table[position] === null || this.table[position] === undefined) {
         this.table[position] = new ValuePair(key, value);
       } else {
+        let index = position + 1;
         while (this.table[index] != null) {
           index++;
         }
@@ -38,4 +39,25 @@ export class HashTableLinearProbing {
     }
     return false;
   }
+
+  get(key) {
+    const position = this.hashCode(key);
+    if (this.table[position] != null) {
+      if (this.table[position].key === key) {
+        return this.table[position].value;
+      }
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key) {
+        index++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        return this.table[index].value;
+      }
+    }
+    return undefined
+  }
 }
+
+const hash = new HashTableLinearProbing();
+hash.put("yt", "@onicolasteofilo");
+hash.put("yt", "@onicolasteofilo2");
