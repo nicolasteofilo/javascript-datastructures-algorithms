7681d98b242c2ef5b9b6ced1eaa76ba9d682317a
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-27 23:35:33 -0300

    feat: add factorialIterative

--- js/data-structures/recursive.js
+++ js/data-structures/recursive.js
@@ -1,12 +1,25 @@
 function recursiveFunction(someParam) {
-    recursiveFunction(someParam)
+  recursiveFunction(someParam);
 }
 
 // recursiveFunction()
 // note: execute in browser this code
 function understandRecursion(doIunderstandRecursion) {
-    const recursionAnswer = confirm("Do you understand recursion?")
-    recursionAnswer ? true : understandRecursion(doIunderstandRecursion) 
+  const recursionAnswer = confirm("Do you understand recursion?");
+  recursionAnswer ? true : understandRecursion(doIunderstandRecursion);
 }
 
-understandRecursion()
+// understandRecursion()
+
+function factorialIterative(number) {
+  if (number < 0) {
+    return undefined;
+  }
+  let total = 1;
+  for (let n = number; n > 1; n--) {
+    total = total * n;
+  }
+  return total
+}
+
+console.log(factorialIterative(5));
