c3021046e7b35aa5a56c31dc6ca774b437af137b
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-27 21:26:21 -0300

    feat: add put method in HashTableLinearProbing

--- js/data-structures/hash-table-linear-probing.js
+++ js/data-structures/hash-table-linear-probing.js
@@ -22,4 +22,20 @@ export class HashTableLinearProbing {
   hashCode(key) {
     return this.loseloseHashCode(key);
   }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      if (this.table[position] == null) {
+        this.table[position] = new ValuePair(key, value);
+      } else {
+        while (this.table[index] != null) {
+          index++;
+        }
+        this.table[index] = new ValuePair(key, value);
+      }
+      return true;
+    }
+    return false;
+  }
 }
