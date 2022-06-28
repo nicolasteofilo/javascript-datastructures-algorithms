6fa30340ad410f05600064bb5039424c916c5a34
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-27 21:16:42 -0300

    feat: add HashTableLinearProbing

---
+++ js/data-structures/hash-table-linear-probing.js
@@ -0,0 +1,25 @@
+import { ValuePair } from "./models/value-pair.js";
+import { defaultToString } from "../utils.js";
+
+export class HashTableLinearProbing {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  loseloseHashCode(key) {
+    if (typeof key === "number") {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+}
