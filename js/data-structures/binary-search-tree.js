b9711a9bead0030e774da49c272f0797f8fee05c
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-29 22:43:46 -0300

    feat: add postOrderTraverse and postOrderTraverseNode in BinarySearchTree

--- js/data-structures/binary-search-tree.js
+++ js/data-structures/binary-search-tree.js
@@ -1,64 +1,95 @@
-import { Compare, defaultCompare } from '../utils.js'
+import { Compare, defaultCompare } from "../utils.js";
 import { Node } from "./models/node.js";
 
 export class BinarySearchTree {
-    constructor(compareFn = defaultCompare) {
-        this.compareFn = compareFn
-        this.root = null
-    }
+  constructor(compareFn = defaultCompare) {
+    this.compareFn = compareFn;
+    this.root = null;
+  }
 
-    insert(key) {
-        if(this.root === null) {
-            this.root = new Node(key)
-        } else {
-            this.insertNode(this.root, key)
-        }
+  insert(key) {
+    if (this.root === null) {
+      this.root = new Node(key);
+    } else {
+      this.insertNode(this.root, key);
     }
+  }
 
-    /** @param {Node} node */
-    insertNode(node, key) {
-        if(this.compareFn(key, node.key) === Compare.LESS_THAN) {
-            if(node.left == null) {
-                node.left = new Node(key)
-            } else {
-                this.insertNode(node.left, key)
-            }
-        } else {
-            if(node.right == null) {
-                node.right = new Node(key)
-            } else {
-                console.log('right recursive')
-                this.insertNode(node.right, key)
-            }
-        }
+  /** @param {Node} node */
+  insertNode(node, key) {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left == null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key);
+      }
+    } else {
+      if (node.right == null) {
+        node.right = new Node(key);
+      } else {
+        console.log("right recursive");
+        this.insertNode(node.right, key);
+      }
     }
+  }
 
-    /**  
-     * @param {Function} callback
-     * @param {Node} node
-    */
-    inOrderTraverseNode(node, callback) {
-        if(node != null) {
-            this.inOrderTraverseNode(node.left, callback)
-            callback(node.key)
-            this.inOrderTraverseNode(node.right, callback)
-        }
+  inOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      this.inOrderTraverseNode(node.right, callback);
     }
+  }
 
-    /**  
-     * @param {Function} callback
-    */
-    inOrderTraverse(callback) {
-        this.inOrderTraverseNode(this.root, callback);
+  preOrderTraverseNode(node, callback) {
+    if (node != null) {
+      callback(node.key);
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
     }
+  }
+
+  postOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.postOrderTraverseNode(node.left, callback);
+      this.postOrderTraverseNode(node.right, callback);
+      callback(node.key);
+    }
+  }
+
+  preOrderTraverse(callback) {
+    this.preOrderTraverseNode(this.root, callback);
+  }
+
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback);
+  }
+
+  postOrderTraverse(callback) {
+    this.postOrderTraverseNode(this.root, callback);
+  }
 }
 
-const tree = new BinarySearchTree()
-tree.insert(11)
-tree.insert(10)
-tree.insert(12)
-tree.insert(8)
+const tree = new BinarySearchTree();
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(6);
+tree.insert(3);
+tree.insert(9);
+tree.insert(8);
+tree.insert(10);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
+tree.insert(20);
+tree.insert(18);
+tree.insert(25);
+
 console.log(tree.root);
 
 const printNode = (value) => console.log(value);
-tree.inOrderTraverse(printNode)
+// tree.inOrderTraverse(printNode);
+// tree.preOrderTraverse(printNode);
+tree.postOrderTraverse(printNode);
