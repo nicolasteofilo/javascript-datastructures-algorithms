169748a9cc521e2ecfedb51dafc9b71a94568491
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-07-06 16:52:52 -0300

    feat: implement remove in binary search tree

--- js/data-structures/binary_search_tree.js
+++ js/data-structures/binary_search_tree.js
@@ -15,7 +15,7 @@ const defaultCompareNumberFn = (a, b) => {
 class TreeNode {
   constructor(value, parent) {
     this.value = value.toString();
-    // this.parent = parent || null;
+    this.parent = parent || null;
     this.left = null;
     this.right = null;
   }
@@ -72,7 +72,6 @@ class BinarySearchTree {
     }
   }
 
-  remove(value, node) {}
   min(node = this.root) {
     while (node && node.left !== null) {
       node = node.left;
@@ -147,7 +146,52 @@ class BinarySearchTree {
   }
 
   search(value) {
-    return this.postOrderTraverse().find((node) => node.value === value.toString())
+    return this.postOrderTraverse().find(
+      (node) => node.value === value.toString()
+    );
+  }
+
+  remove(value, node) {
+    node = node ? node : this.search(value);
+    if (!node) return null;
+
+    const nodeIsRoot = node.parent === null;
+    const nodeBothChildren = node.left !== null && node.right !== null;
+    const isLeftChild = !nodeIsRoot ? node.parent.left === node : false;
+
+    if (node.isLeaf) {
+      if (nodeIsRoot) {
+        this.root = null;
+      } else if (node.parent.left === node) {
+        node.parent.left = null;
+      } else {
+        node.parent.right = null;
+      }
+      return node;
+    }
+    if (!nodeBothChildren) {
+      const child = node.left !== null ? node.left : node.right;
+      if (nodeIsRoot) {
+        this.root = child;
+      } else if (isLeftChild) {
+        node.parent.left = child;
+      } else {
+        node.parent.right = child;
+      }
+      child.parent = node.parent;
+      return node;
+    }
+
+    const minRightLeaf = this.min(node.right);
+
+    if (minRightLeaf.parent.left === minRightLeaf) {
+      minRightLeaf.parent.left = null;
+    } else {
+      minRightLeaf.parent.right = null;
+    }
+    const clone = { ...node };
+    node.value = minRightLeaf.value;
+    return clone;
   }
 }
 
@@ -157,15 +201,17 @@ const tree = new BinarySearchTree();
 tree.insert(11);
 tree.insert(12);
 tree.insert(10);
-tree.insert(9);
-tree.insert(14);
-tree.insert(13);
-tree.insert(15);
-tree.insert(8);
-console.log(JSON.stringify(tree.root, null, 2));
+// tree.insert(9);
+// tree.insert(14);
+// tree.insert(13);
+// tree.insert(15);
+// tree.insert(8);
+console.log(tree.root);
 console.log("min -->", tree.min().value);
 console.log("max -->", tree.max().value);
 console.log("inOrderTraverse -->", tree.inOrderTraverse());
 console.log("preOrderTraverse -->", tree.preOrderTraverse());
 console.log("postOrderTraverse -->", tree.postOrderTraverse());
-console.log("search -->", tree.search(12));
+console.log("search -->", tree.search(11));
+console.log("remove -->", tree.remove(10));
+console.log(tree.root);
