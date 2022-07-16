53c1087262cf511148478e28f15baa668a4719dc
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-07-16 14:53:07 -0300

    feat: implement getBalanceFactor in AVL tree

--- js/data-structures/avl-tree.js
+++ js/data-structures/avl-tree.js
@@ -15,9 +15,28 @@ class AVLTree extends BinarySearchTree {
 
     // if result is not 0,1 or -1, this node is unbalanced
     // this concept is called balance factor
+
+    // the Max.max() return the higher number of parameters passed
     return (
       Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) +
       1
     );
+  }
+
+  getBalanceFactor(node) {
+    const heightDifference =
+      this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+    switch (heightDifference) {
+      case -2:
+        return BalanceFactor.UNBALANCED_RIGHT;
+      case -1:
+        return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+      case 1:
+        return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+      case 2:
+        return BalanceFactor.UNBALANCED_LEFT;
+      default:
+        return BalanceFactor.BALANCED;
+    }
   }
 }
